ade5eb3c131318d328fdf3328281ef92909da0c8
Abiyyu <yafiabiyyu@protonmail.com>
2023-08-16 11:57:10 +0700

    update

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -4,8 +4,8 @@ const NAV_LINKS: ISubNav[] = [
   { label: 'Home', href: '/' },
   {
     label: 'My Collection',
-    href: '/balances/nft'
-  }
+    href: '/balances/nft',
+  },
 ];
 
 export default NAV_LINKS;
