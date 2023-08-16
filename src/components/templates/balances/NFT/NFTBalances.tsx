13d2662bc8b0f5f7e2d3c8b1b1f06342cf4b578c
Abiyyu <yafiabiyyu@protonmail.com>
2023-08-16 12:05:09 +0700

    update

--- src/components/templates/balances/NFT/NFTBalances.tsx
+++ src/components/templates/balances/NFT/NFTBalances.tsx
@@ -1,25 +1,18 @@
 import { Box, Grid, Heading } from '@chakra-ui/react';
-import { useEvmWalletNFTs, useEvmWalletNFTCollections } from '@moralisweb3/next';
+import { useEvmWalletNFTs } from '@moralisweb3/next';
 import { NFTCard } from 'components/modules';
 import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
 import { useNetwork } from 'wagmi';
 
 const NFTBalances = () => {
   const { data } = useSession();
   const { chain } = useNetwork();
 
-  const { data: nftss } = useEvmWalletNFTCollections({
-    address: data?.user?.address,
-    chain: chain?.id,
-  })
   const { data: nfts } = useEvmWalletNFTs({
     address: data?.user?.address,
     chain: chain?.id,
     tokenAddresses: ['0xc52A29194C583b05A6602E1f5f3675AF50095Fa9']
   });
-
-  useEffect(() => console.log('nfts: ', nfts), [nfts]);
 
   return (
     <>
